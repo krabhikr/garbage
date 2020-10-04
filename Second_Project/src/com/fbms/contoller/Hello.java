3d0fc092771214b522c591d9ba144820d49579a1
hp <hp@LAPTOP-GLPGMT03.mshome.net>
2020-10-04 14:00:41 +0530

    second project commit

---
+++ Second_Project/src/com/fbms/contoller/Hello.java
@@ -0,0 +1,11 @@
+package com.fbms.contoller;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to second project");
+
+	}
+
+}
